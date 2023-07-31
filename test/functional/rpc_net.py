a3e96044e916b3ebc4e9901ecfefdca4305fbeee
Jan <125610144+Jongjan88@users.noreply.github.com>
2023-07-31 12:08:11 +0200

    Update port rpc_net.py
    Change port 8333 ( Bitcoin ) to 12024 ( Digibyte )

--- test/functional/rpc_net.py
+++ test/functional/rpc_net.py
@@ -194,7 +194,7 @@ class NetTest(DigiByteTestFramework):
 
         # Add an IPv6 address to the address manager.
         ipv6_addr = "1233:3432:2434:2343:3234:2345:6546:4534"
-        self.nodes[0].addpeeraddress(address=ipv6_addr, port=8333)
+        self.nodes[0].addpeeraddress(address=ipv6_addr, port=12024)
 
         # Add 10,000 IPv4 addresses to the address manager. Due to the way bucket
         # and bucket positions are calculated, some of these addresses will collide.
@@ -220,7 +220,7 @@ class NetTest(DigiByteTestFramework):
             assert_greater_than(a["time"], 1527811200)  # 1st June 2018
             assert_equal(a["services"], services)
             assert a["address"] in imported_addrs
-            assert_equal(a["port"], 8333)
+            assert_equal(a["port"], 12024)
             assert_equal(a["network"], "ipv4")
 
         # Test the IPv6 address.
@@ -228,7 +228,7 @@ class NetTest(DigiByteTestFramework):
         assert_equal(len(res), 1)
         assert_equal(res[0]["address"], ipv6_addr)
         assert_equal(res[0]["network"], "ipv6")
-        assert_equal(res[0]["port"], 8333)
+        assert_equal(res[0]["port"], 12024)
         assert_equal(res[0]["services"], services)
 
         # Test for the absence of onion and I2P addresses.
@@ -249,18 +249,18 @@ class NetTest(DigiByteTestFramework):
         assert "addpeerinfo" in node.help("addpeerinfo")
 
         self.log.debug("Test that adding an empty address fails")
-        assert_equal(node.addpeeraddress(address="", port=8333), {"success": False})
+        assert_equal(node.addpeeraddress(address="", port=12024), {"success": False})
         assert_equal(node.getnodeaddresses(count=0), [])
 
         self.log.debug("Test that adding a valid address succeeds")
-        assert_equal(node.addpeeraddress(address="1.2.3.4", port=8333), {"success": True})
+        assert_equal(node.addpeeraddress(address="1.2.3.4", port=12024), {"success": True})
         addrs = node.getnodeaddresses(count=0)
         assert_equal(len(addrs), 1)
         assert_equal(addrs[0]["address"], "1.2.3.4")
-        assert_equal(addrs[0]["port"], 8333)
+        assert_equal(addrs[0]["port"], 12024)
 
         self.log.debug("Test that adding the same address again when already present fails")
-        assert_equal(node.addpeeraddress(address="1.2.3.4", port=8333), {"success": False})
+        assert_equal(node.addpeeraddress(address="1.2.3.4", port=12024), {"success": False})
         assert_equal(len(node.getnodeaddresses(count=0)), 1)
 
 
